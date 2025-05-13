ff2d86b7d2354271c3eb74493d241eb6d510c876
Vinicius Oliveira <vinicius.oliveira1399@gmail.com>
2025-05-12 22:18:31 -0300

    fix: 5. 1005 fix media peso

--- exercises/5.py
+++ exercises/5.py
@@ -1,7 +1,9 @@
 def calc_media(a:float, b:float):
-    return ((a + b) / 2)
+    nota1 = a * 3.5
+    nota2 = b * 7.5
+    return ((nota1 + nota2) / 11)
 
 a = float(input())
 b = float(input())
 
-print(f"MEDIA = {calc_media(a,b):.4f}")
+print(f"MEDIA = {calc_media(a,b):.5f}")
